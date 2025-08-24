b46687993a58e07571355d98a1c393f86a81c108
gt47ob <gt47ob@inf.elte.hu>
2025-08-24 16:57:05 +0200

    added subscribe page with available options and styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         >
           <ReactQueryClientProvider>
             <NavBar />
-            <div className="max-w-7xl mx-auto pt-16 p-4 min-h-screen">
+            <div className="max-w-7xl mx-auto p-4 min-h-screen">
               {children}
             </div>
           </ReactQueryClientProvider>
---
+++ lib/plans.ts
@@ -0,0 +1,54 @@
+export interface Plan {
+  name: string;
+  amount: number;
+  currency: string;
+  interval: string;
+  isPopular?: boolean;
+  description: string;
+  features: string[];
+}
+
+export const availablePlans: Plan[] = [
+  {
+    name: "Weekly Plan",
+    amount: 1.99,
+    currency: "USD",
+    interval: "week",
+    isPopular: false,
+    description:
+      "Great if you want to try out the service before committing for longer.",
+    features: [
+      "Unlimited AI meal plans",
+      "AI nutrition insights",
+      "Cancel Anytime",
+    ],
+  },
+  {
+    name: "Monthly Plan",
+    amount: 6.99,
+    currency: "USD",
+    interval: "month",
+    isPopular: true,
+    description:
+      "Perfect for ongoing, month-to-month meal planning and features.",
+    features: [
+      "Unlimited AI meal plans",
+      "Priority AI support",
+      "Cancel Anytime",
+    ],
+  },
+  {
+    name: "Yearly Plan",
+    amount: 70,
+    currency: "USD",
+    interval: "year",
+    isPopular: false,
+    description:
+      "Best value for those committed to improving their diet long-term.",
+    features: [
+      "Unlimited AI meal plans",
+      "All premium features",
+      "Cancel Anytime",
+    ],
+  },
+];
